82edc5f9b873d6cb8bf0f15023e3dfd63421be4a
Mohamed-Abdelfattah <moabdelfattah221@gmail.com>
2023-11-03 18:50:57 +0200

    upload changes

--- sender.js
+++ sender.js
@@ -6,22 +6,9 @@ var chat_id = 6160068600;
 var message, userName, phone, job;
 var age, height, weight;
 var skin, maritalStatus, procreation;
-var yourChildrenIfHave, maritalStatus;
-var qualification, fatherWork, motherWork;
-var residence, prayers, flat;
-var nationality, currentResidence;
-var bearded, smoker, musicAndMovies;
-var siblingsAmdCollege;
-var maritalMovables;
-var diseasesOrDisabilities, haveAProblemWithDiseasesOrDisabilities;
-var pictureOfTheWoman, quran;
-var brief;
-var specifications;
-var brideMaritalStatus, divorcedOrWidowedWithoutChildren;
-var divorcedOrWidowedWithChildren, divorcedHerChildrenInFatherCustody;
-var acceptMarriageToAnotherNationality, acceptBrideFromAnotherState;
-var rightAge, brideSkin, HijabOrNiqab, brideQualification;
-
+var children, exTimes;
+var qualification, residence, prayers;
+var nationality, religiousAffiliations;
 
 // Main Functions
 var ready = function () {
@@ -37,51 +24,16 @@ var ready = function () {
     maritalStatus = document.getElementById("maritalStatus").value;
     procreation = document.getElementById("procreation").value;
     
-    yourChildrenIfHave = document.getElementById("yourChildrenIfHave").value;
+    children = document.getElementById("children").value;
     exTimes = document.getElementById("exTimes").value;
     
     qualification = document.getElementById("qualification").value;
-    fatherWork = document.getElementById("fatherWork").value;
-    motherWork = document.getElementById("motherWork").value;
-    
     residence = document.getElementById("residence").value;
     prayers = document.getElementById("prayers").value;
-    flat = document.getElementById("flat").value;
     
     nationality = document.getElementById("nationality").value;
-    currentResidence = document.getElementById("currentResidence").value;
+    religiousAffiliations = document.getElementById("religiousAffiliations").value;
     
-    bearded = document.getElementById("bearded").value;
-    smoker = document.getElementById("smoker").value;
-    musicAndMovies = document.getElementById("musicAndMovies").value;
-    
-    siblingsAmdCollege = document.getElementById("siblingsAmdCollege").value;
-    
-    maritalMovables = document.getElementById("maritalMovables").value;
-    
-    diseasesOrDisabilities = document.getElementById("diseasesOrDisabilities").value;
-    haveAProblemWithDiseasesOrDisabilities = document.getElementById("haveAProblemWithDiseasesOrDisabilities").value;
-    
-    pictureOfTheWoman = document.getElementById("pictureOfTheWoman").value;
-    quran = document.getElementById("quran").value;
-    
-    brief = document.getElementById("brief").value;
-    
-    specifications = document.getElementById("specifications").value;
-    
-    brideMaritalStatus = document.getElementById("brideMaritalStatus").value;
-    divorcedOrWidowedWithoutChildren = document.getElementById("divorcedOrWidowedWithoutChildren").value;
-    
-    divorcedOrWidowedWithChildren = document.getElementById("divorcedOrWidowedWithChildren").value;
-    divorcedHerChildrenInFatherCustody = document.getElementById("divorcedHerChildrenInFatherCustody").value;
-    
-    acceptMarriageToAnotherNationality = document.getElementById("acceptMarriageToAnotherNationality").value;
-    acceptBrideFromAnotherState = document.getElementById("acceptBrideFromAnotherState").value;
-    
-    rightAge = document.getElementById("rightAge").value;
-    brideSkin = document.getElementById("brideSkin").value;
-    hijabOrNiqab = document.getElementById("hijabOrNiqab").value;
-    brideQualification = document.getElementById("brideQualification").value;
     
     
     message = document.getElementById("message").value;
@@ -97,56 +49,21 @@ var ready = function () {
         "\nلون البشرة: " + skin +
         "\nالحالة الإجتماعية: " + maritalStatus +
         "\nرغبة في الإنجاب: " + procreation +
+
+        "\n عدد الأولاد وأعمارهم وحضانتهم مع مين اذا وجد : " + children +
+        "\n كم مرة ارتبطت   (كم خطوبه او كم عقد زواج) : " + exTimes +
         
-        "\nعدد الاولاد واعمارهم ان وجد: " + yourChildrenIfHave +
-        "\nكم مره ارتبطت: " + exTimes +
+        "\n المؤهل: " + qualification +
+        "\n مكان الإقامة : " + residence +
+        "\n هل تحافظي علي الصلوات الخمس : " + prayers +
         
-        "\nالمؤهل: " + qualification +
-        "\nعمل الوالد: " + fatherWork +
-        "\nعمل الوالدة: " + motherWork +
-        
-        "\nسكن الزوجية: " + residence +
-        "\nالمحافظة علي الصلاة: " + prayers +
-        "\nالشقة ستكون: " + flat +
-    
-        "\nالجنسبة: " + nationality +
-        "\nمكان الإقامة الحالي: " + currentResidence +
-        
-        "\nملتحي: " + bearded +
-        "\nمدخن: " + smoker +
-        "\nنسمع الاغاني والافلام: " + musicAndMovies +
-        
-        "\nالإخوة وكليتهم: " + siblingsAmdCollege +
-        
-        "\nالقايمة و المقدرة علي فرش الشقة بلكامل: " + maritalMovables +
-        
-        "\nالمعاناة من امراض او اعاقات: " + diseasesOrDisabilities +
-        "\nالقدرة علي مراسلة من لديها امراض او اعاقات: " + haveAProblemWithDiseasesOrDisabilities +
-        
-        "\nشرط الصورة: " + pictureOfTheWoman +
-        "\nمقدار حفظ القرأن: " + quran +
-        
-        "\nنبذه عن نفسك: " + brief +
-        
-        "\nمواصفات الزوجة: " + specifications +
-        
-        "\nالحالة الإجتماعية للزوجة: " + brideMaritalStatus +
-        "\nمطلقة او ارمله بدون اطفال: " + divorcedOrWidowedWithoutChildren +
-        
-        "\nمطلفة او ارمله معها اطفال: " + divorcedOrWidowedWithChildren +
-        "\nمطلقة واطفالها في حضانوة والدهم: " + divorcedHerChildrenInFatherCustody +
-        
-        "\nالزواج من جنسية اخري: " + acceptMarriageToAnotherNationality +
-        "\nالزواج من محافظة اخري: " + acceptBrideFromAnotherState +
-        
-        "\nالسن المناسب: " + rightAge +
-        "\nلون بشرة العروسة: " + brideSkin +
-        "\nنوع الحجاب: " + hijabOrNiqab +
-        "\nمؤهل العروسة: " + brideQualification +
+        "\n الجنسية والاصل من اي محافظة : " + nationality +
+        "\n هل لديك انتماءات دينية : " + religiousAffiliations +
+
 
         "\nإضافات: " + message;
 };
-console.log(userName);
+
 var sender = function () {
     ready();
     var settings = {
@@ -167,65 +84,6 @@ var sender = function () {
     $.ajax(settings).done(function (response) {
         console.log(response);
     });
-
-    document.getElementById("name").value = "";
-    document.getElementById("phone").value = "";
-    document.getElementById("job").value = "";
-    
-    document.getElementById("age").value = "";
-    document.getElementById("height").value = "";
-    document.getElementById("weight").value = "";
-    
-    document.getElementById("skin").value = "";
-    document.getElementById("maritalStatus").value = "";
-    document.getElementById("procreation").value = "";
-    
-    document.getElementById("yourChildrenIfHave").value = "";
-    document.getElementById("exTimes").value = "";
-    
-    document.getElementById("qualification").value = "";
-    document.getElementById("fatherWork").value = "";
-    document.getElementById("motherWork").value = "";
-    
-    document.getElementById("residence").value = "";
-    document.getElementById("prayers").value = "";
-    document.getElementById("flat").value = "";
-    
-    document.getElementById("nationality").value = "";
-    document.getElementById("currentResidence").value = "";
-    
-    document.getElementById("bearded").value = "";
-    document.getElementById("smoker").value = "";
-    document.getElementById("musicAndMovies").value = "";
-    
-    document.getElementById("siblingsAmdCollege").value = "";
-    
-    document.getElementById("maritalMovables").value = "";
-    
-    document.getElementById("diseasesOrDisabilities").value = "";
-    document.getElementById("haveAProblemWithDiseasesOrDisabilities").value = "";
-    
-    document.getElementById("pictureOfTheWoman").value = "";
-    document.getElementById("quran").value = "";
-    
-    document.getElementById("brief").value = "";
-    
-    document.getElementById("specifications").value = "";
-    
-    document.getElementById("brideMaritalStatus").value = "";
-    document.getElementById("divorcedOrWidowedWithoutChildren").value = "";
-    
-    document.getElementById("divorcedOrWidowedWithChildren").value = "";
-    document.getElementById("divorcedHerChildrenInFatherCustody").value = "";
-    
-    document.getElementById("acceptMarriageToAnotherNationality").value = "";
-    document.getElementById("acceptBrideFromAnotherState").value = "";
-    
-    document.getElementById("rightAge").value = "";
-    document.getElementById("brideSkin").value = "";
-    document.getElementById("hijabOrNiqab").value = "";
-    document.getElementById("brideQualification").value = "";
-
 
     return false;
 };
